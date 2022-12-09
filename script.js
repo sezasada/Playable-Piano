33408c3133f309b54b0a94d88d6810ab455e26f7
sezasada <111367099+sezasada@users.noreply.github.com>
2022-12-09 11:39:43 -0800

    js html and css.
    Update small things. Not done yet.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
 
 let audio = new Audio("tunes/a.wav");
 
@@ -10,3 +11,6 @@ const playTune = (key) => {
 pianoKeys.forEach(key => {
     key.addEventListener("click", () => playTune(key.dataset.key));
 });
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+}
